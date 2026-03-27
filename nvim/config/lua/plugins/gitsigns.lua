a6b53dc675496f4b2e64367e26e6031c835e9a01
Bruno Porto <53821307+Brunoporto2702@users.noreply.github.com>
2026-03-27 01:14:16 -0300

    Add which-key, gitsigns, todo-comments, lualine, bufferline, noice (#13)

---
+++ nvim/config/lua/plugins/gitsigns.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufReadPre",
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "▎" },
+					change = { text = "▎" },
+					delete = { text = "" },
+					topdelete = { text = "" },
+					changedelete = { text = "▎" },
+				},
+				on_attach = function(buffer)
+					local gs = package.loaded.gitsigns
+					local map = function(mode, l, r, desc)
+						vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+					end
+					map("n", "]h", gs.next_hunk, "Next hunk")
+					map("n", "[h", gs.prev_hunk, "Prev hunk")
+					map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+					map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+					map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
+					map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame line")
+				end,
+			})
+		end,
+	},
+}
